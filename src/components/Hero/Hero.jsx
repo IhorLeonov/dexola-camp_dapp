f93b7bc8d2dee5330265b078a6e47e4077363f6c
Ihor Leonov <ihorleonow@gmail.com>
2023-09-11 02:42:12 +0300

    ADD | hero styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,24 @@
 import s from "./Hero.module.scss";
 
 export const Hero = () => {
-  return <section className={s.hero}></section>;
+  return (
+    <section className={s.hero}>
+      <h1 className={s.hero_title}>StarRunner Token staking</h1>
+      <ul className={s.hero_info_list}>
+        <li className={s.hero_info_item}>
+          <span className={s.hero_accent_word}>0.00</span> STRU{" "}
+          <span className={s.hero_info_desc}>Staked balance</span>
+        </li>
+        <li className={s.hero_info_item}>
+          <span className={s.hero_accent_word}>≈8%</span> APY
+        </li>
+        <li className={s.hero_info_item}>
+          <span className={s.hero_accent_word}>0</span> Days
+        </li>
+        <li className={s.hero_info_item}>
+          <span className={s.hero_accent_word}>0</span> STRU Rewards
+        </li>
+      </ul>
+    </section>
+  );
 };
