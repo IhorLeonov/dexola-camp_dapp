e0d50e6f5299e85ca82e9aaf9b24be3f45e588ad
Ihor Leonov <ihorleonow@gmail.com>
2023-09-14 17:01:49 +0300

    FIX | prompts position

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,51 +22,53 @@ export const Hero = () => {
   };
 
   return (
-    <section id="hero" className={s.hero}>
-      <h1 className={s.hero_title}>StarRunner Token staking</h1>
-      <ul className={s.hero_info}>
-        <li className={s.hero_info_balance}>
-          <span className={s.hero_amount}>0.00</span>
-          <span className={s.hero_stru}>STRU</span>{" "}
-          <QuestionMark
-            name="balance"
-            onShowPrompt={handleShowPrompt}
-            onHidePrompt={handleHidePrompt}
-          />
-          <span className={s.hero_info_desc}>Staked balance</span>
-        </li>
+    <section className={s.hero}>
+      <div className={s.hero_container}>
+        <h1 className={s.hero_title}>StarRunner Token staking</h1>
+        <ul className={s.hero_info}>
+          <li className={s.hero_info_balance}>
+            <span className={s.hero_amount}>0.00</span>
+            <span className={s.hero_stru}>STRU</span>{" "}
+            <QuestionMark
+              name="balance"
+              onShowPrompt={handleShowPrompt}
+              onHidePrompt={handleHidePrompt}
+            />
+            <span className={s.hero_info_desc}>Staked balance</span>
+          </li>
 
-        <li className={s.hero_info_apr}>
-          <span className={s.hero_amount}>≈8%</span>
-          <QuestionMark
-            name="apr"
-            onShowPrompt={handleShowPrompt}
-            onHidePrompt={handleHidePrompt}
-          />{" "}
-          <span className={s.hero_info_desc}>APR</span>
-        </li>
+          <li className={s.hero_info_apr}>
+            <span className={s.hero_amount}>≈8%</span>
+            <QuestionMark
+              name="apr"
+              onShowPrompt={handleShowPrompt}
+              onHidePrompt={handleHidePrompt}
+            />{" "}
+            <span className={s.hero_info_desc}>APR</span>
+          </li>
 
-        <li className={s.hero_info_days}>
-          <span className={s.hero_amount}>0</span>{" "}
-          <span className={s.hero_info_desc}>Days</span>
-        </li>
+          <li className={s.hero_info_days}>
+            <span className={s.hero_amount}>0</span>{" "}
+            <span className={s.hero_info_desc}>Days</span>
+          </li>
 
-        <li className={s.hero_info_rewards}>
-          <span className={s.hero_amount}>0</span>{" "}
-          <span className={s.hero_stru}>STRU</span>{" "}
-          <QuestionMark
-            name="rewards"
-            onShowPrompt={handleShowPrompt}
-            onHidePrompt={handleHidePrompt}
-          />
-          <span className={s.hero_info_desc}>Rewards</span>
-        </li>
-      </ul>
-      <Prompt
-        promptClass={promptClass}
-        name={promptName}
-        onHidePrompt={handleHidePrompt}
-      />
+          <li className={s.hero_info_rewards}>
+            <span className={s.hero_amount}>0</span>{" "}
+            <span className={s.hero_stru}>STRU</span>{" "}
+            <QuestionMark
+              name="rewards"
+              onShowPrompt={handleShowPrompt}
+              onHidePrompt={handleHidePrompt}
+            />
+            <span className={s.hero_info_desc}>Rewards</span>
+          </li>
+        </ul>
+        <Prompt
+          promptClass={promptClass}
+          name={promptName}
+          onHidePrompt={handleHidePrompt}
+        />
+      </div>
     </section>
   );
 };
