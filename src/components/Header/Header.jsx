d938f9adeccb744515b944cf115161230438155b
Ihor Leonov <ihorleonow@gmail.com>
2023-09-15 00:30:01 +0300

    ADD | wallet connection

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,39 @@
 import styles from "./Header.module.scss";
 import { Icon } from "../../helpers/IconSelector";
 
+import { useConnect } from "wagmi";
+
 export const Header = () => {
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+
   return (
     <header className={styles.header}>
       <div className={styles.header_container}>
         <a href="https://dexola.com/" target="_blank" rel="noreferrer">
           <Icon id="logo" />
         </a>
-        <button className={styles.header_btn} type="button">
+        {/* <button className={styles.header_btn} type="button">
           Connect wallet
-        </button>
+        </button> */}
+        <div>
+          {connectors.map((connector) => (
+            <button
+              style={{ color: "aqua" }}
+              disabled={!connector.ready}
+              key={connector.id}
+              onClick={() => connect({ connector })}
+            >
+              {connector.name}
+              {!connector.ready && " (unsupported)"}
+              {isLoading &&
+                connector.id === pendingConnector?.id &&
+                " (connecting)"}
+            </button>
+          ))}
+
+          {error && <div>{error.message}</div>}
+        </div>
       </div>
     </header>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./sass/index.scss";
-
 import { BrowserRouter } from "react-router-dom";
+
+import { WagmiConfig, createConfig, configureChains, sepolia } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [sepolia],
+  [
+    alchemyProvider({ apiKey: "NfiZDC6bdVJ3L_js6h3pidOSrKDLfKAM" }),
+    publicProvider(),
+  ]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  publicClient,
+  webSocketPublicClient,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/dexola-camp_dapp"> */}
     <BrowserRouter>
-      <App />
+      <WagmiConfig config={config}>
+        <App />
+      </WagmiConfig>
     </BrowserRouter>
   </React.StrictMode>
 );
